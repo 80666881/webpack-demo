7dc59cfb947f9eb36d1bd9f846197e371ebaef06
zeller <87192329@163.com>
2017-06-29 14:44:21 +0800

    完成webpack的第一个例子

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
+import $ from 'jquery';
+import foo from './foo'
 
 function component (){
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello','webpack'],' ');
-    return element;
+   var element = $('<div><div>');
+
+   element.html(_.join(['Hello','webpack'],' '));
+   return element.get(0);
 }
 
 document.body.appendChild(component());
+console.log(foo());
